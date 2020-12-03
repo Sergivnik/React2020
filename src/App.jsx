e5d8c1dcd19dda564ccf5f612118611f0124ce47
Serg <sergivnik@mail.ru>
2020-12-03 18:53:02 +0300

    v-1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { MessageList } from "./container/messageList/MessageList.jsx";
+import { ButtonPushMe } from "./container/buttonPushMe/BattonPushMe.jsx";
 
 export function App() {
   let messages = [
     { name: "Федор", content: "Привет" },
     { name: "Федор", content: "Как дела?" },
   ];
-  return <MessageList messagesList={messages} />;
+  const [messageState, setMessages] = useState(messages);
+  console.log("messageState", messageState);
+  function handlePush(message) {
+    setMessages([...messageState, message]);
+    //return messages;
+  }
+  return (
+    <div>
+      <ButtonPushMe getPush={handlePush} />
+      <MessageList messagesList={messageState} />
+    </div>
+  );
 }
--- src/container/message/Message.jsx
+++ src/container/message/Message.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./messageStyle.sass";
+
 export function Message(props) {
-  console.log(props.userMessage);
   return (
     <div className="messageDiv">
       <p>
