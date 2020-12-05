86922678b96df0adb86e9f79d8260be8fbb56745
Serg <sergivnik@mail.ru>
2020-12-05 21:23:06 +0300

    v-2.3-2

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ const messages = [
 
 export function App() {
   const [messageState, setMessages] = useState(messages);
+  const [updateRobot, setUpdateRobot] = useState(null);
+  const toggleUpdate = () => setUpdateRobot(!updateRobot);
 
   const handlePush = useCallback(
     (message) => setMessages([...messageState, message]),
@@ -19,24 +21,17 @@ export function App() {
   );
 
   useEffect(() => {
-    // Как-то странно перерендеривается, если отправить несколько сообщений подряд
-    // за 3 сек. В итоге все нормально, но в процессе React наверное с ума сходит))
-    // кстати и в итоге могут данные пропадать. Если неостанавливаясь вводить
-    // 1 enter 2 enter 3 enter 4 enter 5 enter и т.д.
-    clearTimeout(timerID);
-    timerID = setTimeout(() => {
-      const lastMessage = messageState[messageState.length - 1];
-      if (lastMessage.name != ROBOT) {
-        setMessages([
-          ...messageState,
-          {
-            name: ROBOT,
-            content: `Hello ${lastMessage.name}, I'm Robot`,
-          },
-        ]);
-      }
-    }, 3000);
-  }, [messageState]);
+    if (messageState[messageState.length - 1].name != ROBOT) {
+      setMessages([
+        ...messageState,
+        {
+          name: ROBOT,
+          content: `Hello, ${ROBOT} I'm robot`,
+        },
+      ]);
+    }
+    setTimeout(toggleUpdate, 3000);
+  }, [updateRobot]);
 
   return (
     <div>
