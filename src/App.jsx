4e5ab5a4d8ee42b266fa26f7741bcf903ee4a951
Serg <sergivnik@mail.ru>
2020-12-05 21:27:54 +0300

    v-2.4 with clearTimeout

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,15 @@ import React, { useCallback, useEffect, useState } from "react";
 import { MessageList } from "./container/messageList/MessageList.jsx";
 import { MessageBlock } from "./container/buttonPushMe/MessageBlock.jsx";
 
-let timerID;
 const ROBOT = "Robot";
 const messages = [
   { name: ROBOT, content: "Привет" },
   { name: ROBOT, content: "Как дела?" },
 ];
+let timerID;
 
 export function App() {
   const [messageState, setMessages] = useState(messages);
-  const [updateRobot, setUpdateRobot] = useState(null);
-  const toggleUpdate = () => setUpdateRobot(!updateRobot);
 
   const handlePush = useCallback(
     (message) => setMessages([...messageState, message]),
@@ -21,17 +19,24 @@ export function App() {
   );
 
   useEffect(() => {
-    if (messageState[messageState.length - 1].name != ROBOT) {
-      setMessages([
-        ...messageState,
-        {
-          name: ROBOT,
-          content: `Hello, ${ROBOT} I'm robot`,
-        },
-      ]);
-    }
-    setTimeout(toggleUpdate, 3000);
-  }, [updateRobot]);
+    // Как-то странно перерендеривается, если отправить несколько сообщений подряд
+    // за 3 сек. В итоге все нормально, но в процессе React наверное с ума сходит))
+    // кстати и в итоге могут данные пропадать. Если неостанавливаясь вводить
+    // 1 enter 2 enter 3 enter 4 enter 5 enter и т.д.
+    clearTimeout(timerID);
+    timerID = setTimeout(() => {
+      const lastMessage = messageState[messageState.length - 1];
+      if (lastMessage.name != ROBOT) {
+        setMessages([
+          ...messageState,
+          {
+            name: ROBOT,
+            content: `Hello ${lastMessage.name}, I'm Robot`,
+          },
+        ]);
+      }
+    }, 3000);
+  }, [messageState]);
 
   return (
     <div>
