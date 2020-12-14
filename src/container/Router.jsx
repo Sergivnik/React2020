3ff764f9acec259e20996274499472c4582eae39
Serg <sergivnik@mail.ru>
2020-12-14 14:25:39 +0300

    useParams

--- src/container/Router.jsx
+++ src/container/Router.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useParams } from "react-router-dom";
 import { App } from "../App.jsx";
+
+function RouteApp() {
+  const { chatId } = useParams();
+  return <App chatId={chatId}></App>;
+}
 
 export function Router() {
   return (
     <Switch>
       <Route exact path="/" component={App} />
-      <Route
-        exact
-        path="/chat/:chatId/"
-        render={(obj) => <App chatId={Number(obj.match.params.chatId)} />}
-      />
+      <Route exact path="/chat/:chatId/">
+        <RouteApp/>
+      </Route>
       <Route
         exact
         path="/profile/:chatId/"
