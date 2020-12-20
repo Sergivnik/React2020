39dd14e312c360dd74455c29e26217e59fd018fa
Serg <sergivnik@mail.ru>
2020-12-20 19:39:57 +0300

    v-6.6

--- src/index.js
+++ src/index.js
@@ -4,18 +4,22 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Router, Route } from "./container/Router.jsx";
 import { ConnectedRouter } from "connected-react-router";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import initStore, { history } from "./utils/store.js";
 import thunk from "redux-thunk";
 import chatReducer from "./reducers/index";
 
+const { store, persistor } = initStore();
 //const store = createStore(chatReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Provider store={initStore()}>
-    <ConnectedRouter history={history}>
-      <Router />
-    </ConnectedRouter>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <Router />
+      </ConnectedRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,16 +1,26 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { createBrowserHistory } from "history";
 import { routerMiddleware } from "connected-react-router";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import initReducers from "./../reducers";
 import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "geekmessanger",
+  storage,
+  stateReconciler: autoMergeLevel2,
+  whitelist: ["chatReducer"],
+};
 
 export const history = createBrowserHistory();
 
 function initStore() {
   const innitialStore = {};
 
-  return createStore(
-    initReducers(history),
+  const store = createStore(
+    persistReducer(persistConfig, initReducers(history)),
     innitialStore,
     compose(
       applyMiddleware(routerMiddleware(history), thunk),
@@ -19,6 +29,9 @@ function initStore() {
         : () => {}
     )
   );
+  const persistor = persistStore(store);
+
+  return { store, persistor };
 }
 
 export default initStore;
